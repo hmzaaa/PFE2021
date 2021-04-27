66c931f2b8af26698b61cfec4595ee4dcb7747c6
hmzaaa <hamza-charhour@hotmail.fr>
2021-04-27 06:57:08 +0000

    init

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'is_admin'], function() {
+        Route::resource('pages', \App\Http\Controllers\Admin\PageController::class);
+        Route::resource('checklist_groups', \App\Http\Controllers\Admin\ChecklistGroupController::class);
+        Route::resource('checklist_groups.checklists', \App\Http\Controllers\Admin\ChecklistController::class);
+        Route::resource('checklists.tasks', \App\Http\Controllers\Admin\TaskController::class);
+    });
+});
